63d467df83f39ce4d6fcf894fdb2e1ec649dce71
Rezai <alialmas1200@gmail.com>
2022-07-19 15:06:28 +0200

    fertig

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-let aktuellkonto = 100;
-document.querySelector("#aktuellkonto").innerHTML = aktuellkonto + "€";
-
+let konto = 100;
+document.querySelector("#konto").innerHTML = konto + "Euro";
 function run() {
-    const price = Math.round(Math.random() * 5 + 1);
-    document.querySelector("#price").innerHTML = price;
-
-    if(price == 6){
-        aktuellkonto += 5;
-
-    }else{
-        aktuellkonto -= 1;
+  const zahl = Math.round(Math.random() * 5 + 1);
+//   document.querySelector("#zahl").innerHTML = zahl;
+  if (zahl == 6) {
+      konto += 5;
+      document.querySelector(".fa-solid").classList = "fa-solid fa-dice-six";
+    } else if (zahl == 1) {
+        konto = konto - 1;
+        document.querySelector(".fa-solid").classList = "fa-solid fa-dice-one";
+    } else if (zahl == 2) {
+        konto = konto - 1;
+        document.querySelector(".fa-solid").classList = "fa-solid fa-dice-two";
+    } else if (zahl == 3) {
+        konto = konto - 1;
+        document.querySelector(".fa-solid").classList = "fa-solid fa-dice-three";
+    } else if (zahl == 4) {
+        konto = konto - 1;
+        document.querySelector(".fa-solid").classList = "fa-solid fa-dice-four";
+    } else if (zahl == 5) {
+        konto = konto - 1;
+        document.querySelector(".fa-solid").classList = "fa-solid fa-dice-five";
     }
-    document.querySelector("#aktuellkonto").innerHTML = aktuellkonto + "€";
-}
-
-function back() {
-    aktuellkonto = 100;
-    document.querySelector("#aktuellkonto").innerHTML = aktuellkonto + "€";
-}
+    document.querySelector("#konto").innerHTML="Dein aktueller Kontostand " +  konto + "Euro";}
+    function revert() {
+       konto = 100;
+     document.querySelector("#konto").innerHTML = konto + "Euro";
+   }
